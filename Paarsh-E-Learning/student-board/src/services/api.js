3b1ad034cd23ef53cc03e29129fa12b784dd27ce
ArpitaGunjal <arpitagunjal05@gmail.com>
2026-02-04 12:48:42 +0530

    Update api.js

--- Paarsh-E-Learning/student-board/src/services/api.js
+++ Paarsh-E-Learning/student-board/src/services/api.js
@@ -1,6 +1,5 @@
 // src/services/api.js
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 
 // Course API functions
